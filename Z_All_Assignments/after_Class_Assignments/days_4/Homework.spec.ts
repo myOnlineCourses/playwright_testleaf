9adcf38ccb8683797a54075353a3463d38d47623
HassansProjects <hassanerrafiisdet@gmail.com>
2024-02-28 23:52:39 -0500

    Desktop_02/28/24  commit the assignment #1

--- Z_All_Assignments/after_Class_Assignments/days_4/Homework.spec.ts
+++ Z_All_Assignments/after_Class_Assignments/days_4/Homework.spec.ts
@@ -11,15 +11,39 @@ Assignment: 1 Create Lead
 9. Click Save and Verify Leads name created
 //******************/
 
-import {test, chromium} from '@playwright/test'
-test('test Assignment 1 Create Lead', async()=>{
+import { test, expect, chromium } from '@playwright/test'
+test('test Assignment 1 Create Lead', async () => {
+  test.setTimeout(50000);
 
-    const browser = await chromium.launch();
-    const browserContext = await browser.newContext();
-    const page = await browserContext.newPage();
+  const browser = await chromium.launch({ headless: false });
+  const browserContext = await browser.newContext();
+  const page = await browserContext.newPage();
 
-    await page.goto("https://login.salesforce.com");
-    await page.locator("input[id='username']").fill("")
+  await page.goto("https://login.salesforce.com");
+  await page.locator("input[id='username']").fill("herrafiisdet@gmail.com");
+  await page.locator("[id=password]").fill("Foumal3ancer@");
+  page.locator("[id=Login]").click();
+
+  await page.locator("div[class=slds-icon-waffle]").click();
+  await page.locator("button[part=button]").click();
+  await page.locator("//p[text()='Sales']").click();
+  await page.locator("(//span[text()='Leads'])[1]").click();
+  await page.locator("//button[text()='New']").click();
+
+  await page.locator("button[name='salutation']").click();
+  await page.getByText("Mr.").click();
+  await page.locator("input[name='lastName']").fill("Erra");
+  await page.locator("input[name='Company']").fill("CompanyXXX");
+
+  await page.locator("button[name='SaveEdit']").click();
+  await page.waitForTimeout(5000)
+  //Verification   
+  const verifylead = await page.locator("span[class='toastMessage slds-text-heading--small forceActionsText']").isVisible()
+  if (verifylead == true) {
+    console.log("******** The leads created successfully ******** ")
+  } else {
+    console.log("Failed to create a lead")
+  }
 
 })
 

f8f9c0505d8f1718f7726efedf534b4e429d232b
HassansProjects <hassanerrafiisdet@gmail.com>
2024-03-07 17:56:57 -0500

    desktop_030724 using git add-A for first time

--- Z_All_Assignments/after_Class_Assignments/days_6/Assignment_01.spec.ts
+++ Z_All_Assignments/after_Class_Assignments/days_6/Assignment_01.spec.ts
@@ -17,6 +17,8 @@ import { expect, test } from "@playwright/test";
 */
 
 
+
+
 test(`TC_002 Auto Waits`, async ({page}) => {
 await page.goto("https://leafground.com/waits.xhtml");
 const cardToSelect = page.locator(".card").filter({hasText:"Wait for Visibility"});
